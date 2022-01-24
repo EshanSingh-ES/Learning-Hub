ba97aaa3b1d281b25ed650c6cffd02522e758843
Radhagupt <eeeshan789@gmail.com>
2022-01-24 11:11:54 +0530

    Update ContentView.swift

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Namaskar Radhagupt")
+        Text("Namaskar Radhagupt,Bharta")
             .padding()
     }
 }
